ebb992d1a03d57f290e2b93b800ea3898ffe76f9
codemattcode <codemattcode@gmail.com>
2017-02-04 04:47:38 +0000

    cl

--- index.php
+++ index.php
@@ -1,2 +1,28 @@
-<h1>Hello World!</h1>
-<p>this is nice hey?</p>
+
+
+<?php  
+  
+get_header();
+
+  if (have_posts()) :
+    while (have_posts()) : the_post(); 
+?>
+
+
+  <div class="article"><h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <p><?php the_content(); ?></p></div>
+
+<?php 
+  endwhile;
+
+  else : 
+    echo '<p>Page Not Found</p>';
+
+  endif;
+
+
+get_footer();
+
+?>
+
+
