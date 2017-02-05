d33642c1938b22a656462fcea61b442dddc26af1
codemattcode <codemattcode@gmail.com>
2017-02-05 00:09:11 +0000

    add site-nav to footer

--- footer.php
+++ footer.php
@@ -1,7 +1,15 @@
 <footer class="site-footer">
+
+  <div class="site-nav">
+      <ul>
+        <?php wp_nav_menu() ?>
+      </ul>  
+    </div>
+
   <p><?php bloginfo('name'); ?>
     - &copy; <?php echo date('Y') ?>
   </p>
+
 </footer>
 
 </div><!-- /wrapper -->
