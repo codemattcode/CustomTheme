d00dbd20aff68259461c3e653bb9df961ecd0279
codemattcode <codemattcode@gmail.com>
2017-02-13 18:50:02 +0000

    add special template

---
+++ special-template.php
@@ -0,0 +1,33 @@
+<?php  
+
+/*
+Template Name: Special Layout
+*/ 
+
+  
+get_header();
+
+  if (have_posts()) :
+    while (have_posts()) : the_post(); 
+?>
+
+
+  <article class="post page">
+    <h2><?php the_title(); ?></h2>
+    <p><?php the_content(); ?></p>
+  </article>
+
+<?php   
+  endwhile;
+
+  else : 
+    echo '<p>Page Not Found</p>';
+
+  endif;
+
+
+get_footer();
+
+?>
+
+
