4f0e59e8677179ee3dcc2c871d13d68500bf1ddd
codemattcode <codemattcode@gmail.com>
2017-02-13 10:53:23 +0000

    cl

--- header.php
+++ header.php
@@ -29,9 +29,12 @@
     
       <div class="wrapper">
 
+  <div class="row">
+    <div class="col-12">    
     <h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
         <h2><?php bloginfo('description') ?></h2>
-
+    </div>
+  </div>
 
     <h3>
     <?php if (is_page('about')) { ?>
