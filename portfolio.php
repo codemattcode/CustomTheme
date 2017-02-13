298e37adf5ca0d39d51976d256bff8d87621777d
codemattcode <codemattcode@gmail.com>
2017-02-13 08:38:36 +0000

    cl

--- portfolio.php
+++ portfolio.php
@@ -1,1 +1,31 @@
-portfolio.php
+
+
+<?php  
+  
+get_header();
+
+  if (have_posts()) :
+    while (have_posts()) : the_post(); 
+?>
+
+
+  <article class="post page">
+    <h2><?php the_title(); ?></h2>
+    <p><?php the_content(); ?></p>
+  </article>
+
+<?php   
+  endwhile;
+
+  else : 
+    echo '<p>Page Not Found</p>';
+
+  endif;
+
+
+get_footer();
+
+?>
+
+
+
