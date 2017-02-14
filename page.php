a04868a3eddc3cd6958df3d3b3739ca5212e1270
codemattcode <codemattcode@gmail.com>
2017-02-14 21:06:47 +0000

    cl

--- page.php
+++ page.php
@@ -9,9 +9,9 @@ get_header();
 ?>
 
 
-  <article class="post page-copy">
+  <article class="post">
     <h2 class="page-title"><?php the_title(); ?></h2>
-    <p><?php the_content(); ?></p>
+    <p class="page-copy"><?php the_content(); ?></p>
   </article>
 
 <?php   
