5a9b99e99138b3ea474b8f9f285c6bfa865ab16a
codemattcode <codemattcode@gmail.com>
2017-02-14 18:13:34 +0000

    cl

--- special-template.php
+++ special-template.php
@@ -14,10 +14,10 @@ get_header();
 
   <!-- <article class="post page">
        </article> -->
+<h1>Template page = 'special-template'</h1>
 
 <h2><?php the_title(); ?></h2>
 
-<h5>Template page = 'special-template'</h5>
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
