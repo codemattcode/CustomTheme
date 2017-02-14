9566dc1b81bedb5a7cc1f1de46789205c2d4b23a
codemattcode <codemattcode@gmail.com>
2017-02-14 18:12:03 +0000

    cl

--- page-portfolio.php
+++ page-portfolio.php
@@ -16,13 +16,22 @@ get_header();
  -->
 
 <div class="row">
+  <div class="col-12"><h1>'Special Page'</h1></div>
+</div>
+
+<div class="row">
   <div class="col-6">
     <h2><?php the_title(); ?></h2>
     <p><?php the_content(); ?></p>
   </div>
  
   <div class="col-6 test">
-    <p><h5>'Special Page'</h5></p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
   </div>
 </div>
 
