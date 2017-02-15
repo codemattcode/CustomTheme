937813c2555243361c904ba2a6d4c30614a2541a
codemattcode <codemattcode@gmail.com>
2017-02-15 02:45:11 +0000

    cl

--- index.php
+++ index.php
@@ -9,18 +9,20 @@ get_header();
 ?>
 
 
-<article class="post has-image">
+<article class="post">
+
 <div class="row">
     <div class="col-6">
 <!-- add post title link / title -->
-    <h2><a class="post-title" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <h2><a class="post-title <?php if ( has_post_thumbnail() ) { ?> <?php } ?>" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
 <!-- add feature image thumbnail -->
     
     <div class="post-thumbnail"><?php the_post_thumbnail('small-thumbnail'); ?></div>
-</div> 
+</div>
 
     <div class="col-6">
+<!-- add post excerpt/content -->
     <p><?php echo get_the_excerpt(); ?>
       <a href="<?php the_permalink(); ?>">Read More &raquo;</a>
     </p>
