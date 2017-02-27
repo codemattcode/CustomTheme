ad287df93b745e98c588d6ffc7cbdc6320826a07
codemattcode <codemattcode@gmail.com>
2017-02-27 17:04:31 +0000

    cl

--- footer.php
+++ footer.php
@@ -1,25 +1,6 @@
 <footer class="site-footer">
 
-<!-- conditional logic whether to display this custom content -->
 
-    <?php if (get_theme_mod('ct-footer-callout-display') == "Yes") { ?>
-    
-        <div class="footer-callout clearfix">
-          <div class="footer-callout-image">
-            <img src="<?php echo wp_get_attachment_url(get_theme_mod('ct-footer-callout-image')) ?>">
-          </div>
-        
-        <div class="footer-callout-text">
-          <h2><a href="<?php echo get_permalink(get_theme_mod('ct-footer-callout-link')) ?>">
-          <?php echo get_theme_mod('ct-footer-callout-headline') ?>
-          </a></h2>
-          <?php echo wpautop(get_theme_mod('ct-footer-callout-text')) ?>
-        </div>
-        </div></?php>
-
-    <?php } ?>
-
-<!-- /custom content -->
 
     <nav class="site-nav nav-footer">
       
--- front-page.php
+++ front-page.php
@@ -7,6 +7,31 @@ get_header();
 
   the_content();
 ?>
+<div class="wrapper">
+<div class="row">
+<div class="col-12">
+<!-- conditional logic whether to display this custom content -->
+
+    <?php if (get_theme_mod('ct-footer-callout-display') == "Yes") { ?>
+    
+        <div class="footer-callout clearfix">
+          <div class="footer-callout-image">
+            <img src="<?php echo wp_get_attachment_url(get_theme_mod('ct-footer-callout-image')) ?>">
+          </div>
+        
+        <div class="footer-callout-text">
+          <h2><a href="<?php echo get_permalink(get_theme_mod('ct-footer-callout-link')) ?>">
+          <?php echo get_theme_mod('ct-footer-callout-headline') ?>
+          </a></h2>
+          <?php echo wpautop(get_theme_mod('ct-footer-callout-text')) ?>
+        </div>
+        </div></?php>
+
+    <?php } ?>
+
+</div>
+</div>
+</div><!-- /wrapper -->
 
 <?php if (is_active_sidebar('widget1')) : ?>
     <div class="widget">
